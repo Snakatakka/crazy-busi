71557d1420ea4014016ae7fe390943d48f0038f1
Snakatakka <etwork563@gmail.com>
2026-03-08 14:13:12 -0400

    added player character functionality

---
+++ scenes/player/bus.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+@export var speed = 0
+@export var maxSpeed = 1200
+@export var minSpeed = -1200
+var rotationSpeed = 0
+
+var rotation_direction = 0
+
+func get_input():
+	if speed != 0:
+		rotation_direction = Input.get_axis("left", "right")
+	
+	if Input.is_action_pressed("up"):
+		if speed < maxSpeed:
+			speed += 4.0
+	elif Input.is_action_pressed("down"):
+		if speed > minSpeed:
+			speed -= 4.0
+	else:
+		if speed != 0:
+			if speed > 0:
+				speed -= 4.0
+			else:
+				speed += 4.0
+	
+	velocity = transform.x * speed
+
+func _physics_process(delta):
+	if speed * 0.1 < 3:
+		rotationSpeed = speed * 0.1
+	else:
+		rotationSpeed = 3
+	
+	get_input()
+	rotation += rotation_direction * (rotationSpeed) * delta
+	move_and_slide()
